530086a0fffce2112ec968df5df354e8d89cf6f2
sdubourg <sdubourg@tenable.com>
2023-07-22 00:31:52 +0200

    feat - implement crude statbar

---
+++ src/components/gl/Person/Statbar.tsx
@@ -0,0 +1,64 @@
+import { useFrame } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import { BufferGeometry, Mesh } from "three";
+
+const vertexShader = `
+    varying vec2 vUv;
+    void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+`;
+
+const fragmentShader = `
+    uniform float percent;
+    varying vec2 vUv;
+    void main() {
+
+        if (vUv.x > percent && vUv.y > 0.05 && vUv.y < 0.95 && vUv.x < 0.99) {
+
+            discard;
+
+        }
+
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+`;
+
+export const Statbar = () => {
+  const ref = useRef<Mesh<BufferGeometry, THREE.ShaderMaterial>>(null);
+
+  const uniforms = useMemo(
+    () => ({
+      percent: { value: 0.01 },
+    }),
+    []
+  );
+
+  useFrame(({ camera }) => {
+    if (!ref.current) {
+      return;
+    }
+
+    ref.current.lookAt(camera.position);
+
+    // update stat
+
+    // TODO : add bounds + decrease on event only
+    ref.current.material.uniforms.percent.value =
+      ref.current.material.uniforms.percent.value + 0.001;
+
+    ref.current.material.uniformsNeedUpdate = true;
+  });
+
+  return (
+    <mesh ref={ref} position-y={4}>
+      <planeBufferGeometry args={[1.5, 0.2]} />
+      <shaderMaterial
+        uniforms={uniforms}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
+    </mesh>
+  );
+};
