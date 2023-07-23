8d56bd64e51b346d42d764a380e7e1efd2f66598
sdubourg <sdubourg@tenable.com>
2023-07-23 03:31:03 +0200

    chore - add leva gui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { useMachine } from "@xstate/react";
-import { Scene } from "./components/gl/Scene";
-import { GameMachineProvider } from "./hooks/use";
-import { gameMachine } from "./machines/game.machine";
-import "./style/App.css";
+import { useMachine } from '@xstate/react';
+import { Scene } from './components/gl/Scene';
+import { GameMachineProvider } from './hooks/use';
+import { gameMachine } from './machines/game.machine';
+import './style/App.css';
+import { button, useControls } from 'leva';
 
 function App() {
   const [state, _, service] = useMachine(gameMachine);
+  const leva = useControls({
+    'Display machines JSON': false,
+    'Add Person': button(() => service.send({ type: 'onAddPerson' })),
+    'Remove Person': button(() =>
+      service.send({ type: 'onRemovePerson', id: '1' })
+    ),
+  });
 
   return (
     <GameMachineProvider value={service}>
-      {JSON.stringify(state.context)}
-      <button onClick={() => service.send({ type: "onAddPerson" })}>
-        add person
-      </button>
-      <button onClick={() => service.send({ type: "onRemovePerson", id: "1" })}>
-        remove person
-      </button>
+      {leva['Display machines JSON'] && JSON.stringify(state.context)}
+
       <Scene />
     </GameMachineProvider>
   );
