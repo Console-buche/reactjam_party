6c00329f710377da73c99977d4339e896509174e
sdubourg <sdubourg@tenable.com>
2023-07-26 23:37:35 +0200

    feat - clamp cam bounds

--- src/components/gl/World/Cam.tsx
+++ src/components/gl/World/Cam.tsx
@@ -1,4 +1,5 @@
 import { CameraControls } from '@react-three/drei';
+import { useControls } from 'leva';
 import { useEffect, useRef } from 'react';
 import { Box3, Vector3 } from 'three';
 import { DEG2RAD } from 'three/src/math/MathUtils';
@@ -6,25 +7,9 @@ import { DEG2RAD } from 'three/src/math/MathUtils';
 export const Cam = () => {
   const cameraControlsRef = useRef<CameraControls>(null);
 
-  // DEBUG
-  // const { d, t } = useControls({
-  //   d: {
-  //     step: 0.1,
-  //     value: 22.95,
-  //     min: 22,
-  //     max: 28,
-  //   },
-  //   t: {
-  //     step: 0.1,
-  //     value: 85,
-  //     min: 60,
-  //     max: 100,
-  //   },
-  // });
-
   useEffect(() => {
     cameraControlsRef.current?.setBoundary(
-      new Box3(new Vector3(-40, 2, 0), new Vector3(80, 2, 0)),
+      new Box3(new Vector3(-26, 2, 0), new Vector3(55.5, 2, 0)),
     );
   }, [cameraControlsRef]);
 
@@ -32,11 +17,9 @@ export const Cam = () => {
     <>
       <CameraControls
         ref={cameraControlsRef}
-        // enabled={!isDragging}
         maxDistance={23}
         distance={23}
         truckSpeed={4}
-        // truckSpeed={isDragging ? -4 : 4}
         polarAngle={DEG2RAD * 87}
         minPolarAngle={DEG2RAD * 87}
         maxPolarAngle={DEG2RAD * 87}
