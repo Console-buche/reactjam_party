d5b6e7e9281fe2d3c1c1277af13e83896f908473
neolectron <xeolectron@gmail.com>
2023-07-26 04:03:53 +0200

    feat: add clock to HUD

--- src/components/hud/Hud.tsx
+++ src/components/hud/Hud.tsx
@@ -1,9 +1,15 @@
+import { useSelector } from '@xstate/react';
+import { useGameMachineProvider } from '../../hooks/use';
 import { Clock } from './clock/Clock';
 
 export const Hud = () => {
+  const gameService = useGameMachineProvider();
+  const timerPercent = useSelector(gameService, (state) => state.context.clock);
+  console.log('timerPercent', timerPercent, Math.random());
+
   return (
     <>
-      <Clock percent={50} />
+      <Clock percent={timerPercent} />
     </>
   );
 };
