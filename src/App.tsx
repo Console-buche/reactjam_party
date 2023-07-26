5b7339ecd3d07ec6877584ea874ec0830e3b3dbd
neolectron <xeolectron@gmail.com>
2023-07-26 02:52:01 +0200

    feat: add game clock

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,20 @@ import { gameMachine } from './machines/game.machine';
 
 import './style/App.css';
 import { Scene } from './components/gl/Scene/Scene';
+import { Hud } from './components/hud/Hud';
 
 function App() {
   const [state, , service] = useMachine(gameMachine);
   const leva = useControls({
     'Display machines JSON': false,
     'Invite Person': button(() => service.send({ type: 'onAddPerson' })),
-    'Remove Last Person': button(() =>
-      service.send({ type: 'onRemoveLastPerson' }),
-    ),
   });
 
   return (
     <GameMachineProvider value={service}>
       {leva['Display machines JSON'] && JSON.stringify(state.context)}
-      {/* <Hud /> */}
       <Scene />
+      <Hud />
     </GameMachineProvider>
   );
 }
--- src/components/hud/Hud.tsx
+++ src/components/hud/Hud.tsx
@@ -1,21 +1,9 @@
-import style from './Hud.module.css';
+import { Clock } from './clock/Clock';
 
 export const Hud = () => {
   return (
-    <div className={style.hud__container}>
-      <section className={style.hud__section}>
-        <h3 className={style.hud__container__title}>
-          New people have arrived!
-        </h3>
-        <div className={style.hud__item}>
-          <div className={style.hud__item__label}>Stats</div>
-          <div className={style.hud__item__value}>🍻 | 🌊 | 🔋 | 🗣️</div>
-        </div>
-        <div className={style.hud__item}>
-          <div className={style.hud__item__label}>Person name...</div>
-          <div className={style.hud__item__value}>100 | 40 | 36 | 30</div>
-        </div>
-      </section>
-    </div>
+    <>
+      <Clock percent={50} />
+    </>
   );
 };
