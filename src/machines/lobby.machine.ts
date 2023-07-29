6cfbf2b41dba95fad44db345aea3cb64bc739295
neolectron <xeolectron@gmail.com>
2023-07-29 04:48:55 +0200

    fix: remove needs to specify updatePersons for lobby

--- src/machines/lobby.machine.ts
+++ src/machines/lobby.machine.ts
@@ -1,7 +1,15 @@
 import { hotspotMachine } from './hotspot.machine';
 
-export const lobbyMachine = hotspotMachine.withContext({
-  ...hotspotMachine.context,
-  maxPersons: 3,
-  name: 'lobby',
-});
+export const lobbyMachine = hotspotMachine
+  .withContext({
+    ...hotspotMachine.context,
+    maxPersons: 3,
+    name: 'lobby',
+  })
+  .withConfig({
+    actions: {
+      updatePersons: (context) => {
+        return context;
+      },
+    },
+  });
