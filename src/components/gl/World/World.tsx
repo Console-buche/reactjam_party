5834b04b20b612d7e89348cbae2b7825b943ae26
sdubourg <sdubourg@tenable.com>
2023-07-29 13:12:53 +0200

    chore:cleanup world

--- src/components/gl/World/World.tsx
+++ src/components/gl/World/World.tsx
@@ -1,9 +1,7 @@
-import { Box } from '@react-three/drei';
 import { useSelector } from '@xstate/react';
 import { Suspense, useRef } from 'react';
 import { type Mesh } from 'three';
 import { useGameMachineProvider } from '../../../hooks/use';
-import { useStoreHotspot } from '../../../stores/storeHotspots';
 import { AppartmentV6 } from '../House/Appartment_v6';
 import { Person } from '../Person/Person';
 import { PostProcess } from '../postProcess/PostProcess';
@@ -14,23 +12,23 @@ export const World = () => {
   const gameService = useGameMachineProvider();
   const persons = useSelector(gameService, (state) => state.context.persons);
 
-  const dropZones = useStoreHotspot((state) => state.hotspots);
+  // debug dropzones, keep while feature hasn't been bullet proofed
+  // const dropZones = useStoreHotspot((state) => state.hotspots);
 
-  const zones = Object.entries(dropZones).flatMap(([_, data]) =>
-    data.dropzones.map((zone) => zone.position),
-  );
+  // const zones = Object.entries(dropZones).flatMap(([_, data]) =>
+  //   data.dropzones.map((zone) => zone.position),
+  // );
 
   return (
     <group>
       <Cam />
 
-      {/* Debug dropzones */}
+      {/* Debug dropzones
       {zones.map((pos, i) => (
         <Box position={pos} key={i} />
-      ))}
+      ))} */}
 
       <AppartmentV6 ref={refFloor} position-y={-0.5} />
-      {/* <DropZones /> */}
       <Suspense fallback={null}>
         {persons.map((actor) => (
           <Person key={actor.id} refFloor={refFloor} actor={actor} />
