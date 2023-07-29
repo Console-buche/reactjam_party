cc5a35da2862daa89c9254d751a50d5269105ac1
neolectron <xeolectron@gmail.com>
2023-07-29 06:23:13 +0200

    chore: remove hostpot logging

--- src/machines/toilet.machine.ts
+++ src/machines/toilet.machine.ts
@@ -9,7 +9,7 @@ export const toiletMachine = hotspotMachine
   .withConfig({
     actions: {
       updatePersons: (context) => {
-        console.log('toilet.updatePersons', context.persons);
+        // console.log('toilet.updatePersons', context.persons);
         context.persons.forEach((p) => p.send('onToilet'));
         return context;
       },
