a6af2120157bdb03f079e3f895dd8022b2f68eca
neolectron <xeolectron@gmail.com>
2023-07-30 20:00:28 +0200

    chore: update game icon

--- src/machines/game.machine.ts
+++ src/machines/game.machine.ts
@@ -66,7 +66,8 @@ export const gameMachine = createMachine({
       on: {
         onStart: {
           target: 'playing',
-        },
+          actions: send('onAddPerson')
+        }
       },
     },
     playing: {
