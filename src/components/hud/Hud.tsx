c1d9f9b623dcd4fb491ac0ccadba7fe74c6343ae
neolectron <xeolectron@gmail.com>
2023-07-30 06:12:10 +0200

    feat: add Main menu

--- src/components/hud/Hud.tsx
+++ src/components/hud/Hud.tsx
@@ -4,9 +4,11 @@ import { Advisor } from './Advisor/Advisor';
 import { GameMeter } from './GameMeter/GameMeter';
 import { HotSpots } from './Hotspots/HotSpots';
 import { Audio } from './Audio/Audio';
+import { Menu } from './Menu/Menu';
 
 export const Hud = () => {
   const gameService = useGameMachineProvider();
+  const state = useSelector(gameService, (state) => state.value);
   const timerPercent = useSelector(gameService, (state) => state.context.clock);
   const hype = useSelector(gameService, (state) => state.context.meters.hype);
   const guideText = useSelector(
@@ -25,6 +27,7 @@ export const Hud = () => {
           console.log('skip text');
         }}
       />
+      {state !== 'playing' && <Menu />}
     </>
   );
 };
---
+++ src/components/hud/Menu/Menu.tsx
@@ -0,0 +1,43 @@
+import { useSelector } from '@xstate/react';
+import { useGameMachineProvider } from '../../../hooks/use';
+import './menu.css';
+
+export const Menu = () => {
+  const gameService = useGameMachineProvider();
+  const state = useSelector(gameService, (state) => state.value);
+
+  const menuItems = [
+    {
+      name: state === 'notStarted' ? 'Start Game' : 'Resume',
+      action: () => gameService.send('onStart'),
+    },
+    { name: 'How to play', action: () => console.log('how to play') },
+    {
+      name: 'Github',
+      action: () =>
+        window.open(
+          'https://github.com/Console-buche/reactjam_party',
+          '_blank',
+        ),
+    },
+  ];
+
+  return (
+    <div className="menu__container">
+      <div className="menu__absolute__wrapper">
+        <img
+          src="./assets/images/invite.png"
+          alt="invitation"
+          className="menu__image"
+        />
+        <div className="menu__list">
+          {menuItems.map((item) => (
+            <div key={item.name} className="menu__item" onClick={item.action}>
+              {item.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
