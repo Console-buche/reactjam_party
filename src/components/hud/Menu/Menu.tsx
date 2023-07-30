d21fb1e6c8f63cce3835f928b5889d8ed6db462f
neolectron <xeolectron@gmail.com>
2023-07-30 22:39:37 +0200

    feat: allow menu customization

--- src/components/hud/Menu/Menu.tsx
+++ src/components/hud/Menu/Menu.tsx
@@ -1,41 +1,21 @@
-import { useSelector } from '@xstate/react';
-import { useGameMachineProvider } from '../../../hooks/use';
 import './menu.css';
+import { useMenuItems } from './useMenuItems';
 
 export const Menu = () => {
-  const gameService = useGameMachineProvider();
-  const state = useSelector(gameService, (state) => state.value);
-
-  const menuItems = [
-    {
-      name: state === 'notStarted' ? 'Start Game' : 'Resume',
-      action: () => gameService.send('onStart'),
-    },
-    { name: 'How to play', action: () => console.log('how to play') },
-    {
-      name: 'Github',
-      action: () =>
-        window.open(
-          'https://github.com/Console-buche/reactjam_party',
-          '_blank',
-        ),
-    },
-  ];
+  const menuItems = useMenuItems();
 
   return (
     <div className="menu__container">
       <div className="menu__absolute__wrapper">
-        <img
-          src="./assets/hud/menu.png"
-          alt="invitation"
-          className="menu__image"
-        />
+        <img src={menuItems.image} alt="invitation" className="menu__image" />
         <div className="menu__list">
-          {menuItems.map((item) => (
-            <div key={item.name} className="menu__item" onClick={item.action}>
-              {item.name}
-            </div>
-          ))}
+          {menuItems.items.map((item) => {
+            return (
+              <div key={item.name} className="menu__item" onClick={item.action}>
+                {item.name}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/components/hud/Menu/useMenuItems.ts
@@ -0,0 +1,55 @@
+import { useGameMachineProvider } from "../../../hooks/use";
+import { useSelector } from "@xstate/react";
+
+const githubItem = {
+  name: 'Github',
+  action: () =>
+    window.open('https://github.com/Console-buche/reactjam_party', '_blank'),
+};
+
+const howToPlayItem = {
+  name: 'How to play',
+  action: () => console.log('how to play'),
+};
+
+export const useMenuItems = () => {
+  const gameService = useGameMachineProvider();
+  const state = useSelector(gameService, (state) => state.value);
+
+  const menuItemsByStates = {
+    notStarted: {
+      image: './assets/hud/menu.png',
+      items: [
+        {
+          name: 'Start Game',
+          action: () => gameService.send('onStart'),
+        },
+        howToPlayItem,
+        githubItem,
+      ],
+    },
+    paused: {
+      image: './assets/hud/menu.png',
+      items: [
+        {
+          name: 'Resume',
+          action: () => gameService.send('onStart'),
+        },
+        howToPlayItem,
+        githubItem,
+      ],
+    },
+    finished: {
+      image: './assets/hud/menu.png',
+      items: [
+        {
+          name: 'Restart',
+          action: () => window.location.reload(),
+        },
+        howToPlayItem,
+        githubItem,
+      ],
+    },
+  };
+  return menuItemsByStates[state as keyof typeof menuItemsByStates];
+}
