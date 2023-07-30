edfc4e764d3c49e2d91d2e2f67a5043b2dbde185
sdubourg <sdubourg@tenable.com>
2023-07-30 10:31:28 +0200

    fix: person scale y

--- src/components/gl/Person/Person.tsx
+++ src/components/gl/Person/Person.tsx
@@ -99,7 +99,7 @@ export const Person = ({
   // setup easings
   const { glow, scale, followSpeed, opacity } = useSpring({
     glow: selectFeedbackIntensiry(isHovered, isBeingDragged),
-    scale: isBeingDragged ? [0.8, 0.9, 0.8] : [0.7, 0.8, 0.7],
+    scale: isBeingDragged ? [0.8, 0.95, 0.8] : [0.7, 0.8, 0.7],
     opacity: isBeingDragged ? 0.5 : 1,
     followSpeed: isBeingDragged ? 0.75 : 0,
   });
