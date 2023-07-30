ffd1040d7cb5a490074e81dcf048760478a3683d
neolectron <xeolectron@gmail.com>
2023-07-30 06:09:28 +0200

    feat: use the escape key to pause/resume

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { gameMachine } from './machines/game.machine';
 import './style/App.css';
 import { Scene } from './components/gl/Scene/Scene';
 import { Hud } from './components/hud/Hud';
+import { useEffect } from 'react';
 
 function App() {
   const [state, , service] = useMachine(gameMachine, {
     devTools: true,
     id: 'GameService',
   });
+
   const leva = useControls({
     'Display machines JSON': false,
     'Invite Person': button(() => service.send({ type: 'onAddPerson' })),
@@ -19,6 +21,24 @@ function App() {
     Pause: button(() => service.send({ type: 'onPause' })),
     'End Game': button(() => service.send({ type: 'onGameOver' })),
   });
+
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+      console.log('User pressed: ', event.key);
+
+      if (event.key === 'Escape') {
+        event.preventDefault();
+        service.getSnapshot().value === 'playing'
+          ? service.send({ type: 'onPause' })
+          : service.send({ type: 'onStart' });
+      }
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, [service]);
 
   return (
     <GameMachineProvider value={service}>
