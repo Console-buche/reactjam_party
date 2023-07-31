d5a65192a632ef75de77cc90dd73d574247c8970
neolectron <xeolectron@gmail.com>
2023-07-31 08:03:54 +0200

    feat: add more sound-effects

--- src/components/hud/Audio/SoundEffect.tsx
+++ src/components/hud/Audio/SoundEffect.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useGameMachineProvider } from '../../../hooks/use';
 import Wad from 'web-audio-daw';
+import { useSelector } from '@xstate/react';
 
 const soundEffects = {
   onAddPerson: new Wad({
@@ -13,13 +14,61 @@ const soundEffects = {
     source: '/assets/sounds/onRemovePerson.mp3',
     volume: 0.3,
   }),
+  bar: new Wad({
+    //@ts-ignore (it works, but the types are wrong)
+    source: '/assets/sounds/bar.mp3',
+    volume: 0.3,
+  }),
+  buffet: new Wad({
+    //@ts-ignore (it works, but the types are wrong)
+    source: '/assets/sounds/buffet.mp3',
+    volume: 0.3,
+    offset: 1.1,
+  }),
+  dancefloor: new Wad({
+    //@ts-ignore (it works, but the types are wrong)
+    source: '/assets/sounds/dancefloor.mp3',
+    volume: 0.3,
+  }),
+  toilet: new Wad({
+    //@ts-ignore (it works, but the types are wrong)
+    source: '/assets/sounds/toilet.mp3',
+    volume: 0.3,
+  }),
+  sofa: new Wad({
+    //@ts-ignore (it works, but the types are wrong)
+    source: '/assets/sounds/sofa.mp3',
+    volume: 0.3,
+  }),
 } as const;
 
 export const SoundEffect = () => {
   const gameService = useGameMachineProvider();
+  const hotspots = useSelector(gameService, (state) => state.context.hotspots);
+
+  useEffect(() => {
+    Object.keys(hotspots).forEach((key) => {
+      const currentHotspot = hotspots[key as keyof typeof hotspots];
+      console.log(currentHotspot.id);
+      currentHotspot.subscribe((state) => {
+        // console.log(state.event.type, 'triggered');
+        if (state.event.type !== 'onRegisterPerson') return;
+
+        const name = currentHotspot.getSnapshot()?.context.name as
+          | keyof typeof soundEffects
+          | undefined;
+
+        if (name === undefined) return;
+
+        const currentSoundEffect = soundEffects[name];
+        currentSoundEffect?.play();
+      });
+    });
+  }, [hotspots]);
 
   useEffect(() => {
     gameService.subscribe((state) => {
+      // console.log(state.event.type);
       if (!soundEffects[state.event.type as keyof typeof soundEffects]) return;
       console.log(state.event.type, 'triggered');
       const currentSoundEffect =
@@ -28,5 +77,6 @@ export const SoundEffect = () => {
       currentSoundEffect.play();
     });
   }, [gameService]);
+
   return null;
 };
