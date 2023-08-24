3f4232e7067f189edecf9e6598ed68c9564bc501
sdubourg <sdubourg@tenable.com>
2023-08-24 10:29:21 +0200

    feat: update pos and style of counter

--- src/components/hud/Clock/Clock.tsx
+++ src/components/hud/Clock/Clock.tsx
@@ -6,7 +6,7 @@ type Props = {
   elapsedTime: number;
 };
 
-function secondsToMinutesAndSeconds(seconds: number) {
+function formatSecondsToMinutesAndSeconds(seconds: number) {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
   return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
@@ -22,7 +22,9 @@ export const Clock = ({ elapsedPercent, elapsedTime, maxTime }: Props) => {
           alt="meter"
           style={{ filter: `grayscale(1)` }}
         />
-        {secondsToMinutesAndSeconds(maxTime - elapsedTime)}
+        <div className="gameMeter__timeLeft">
+          {formatSecondsToMinutesAndSeconds(maxTime - elapsedTime)}
+        </div>
         <img
           src="./assets/hud/clock.png"
           alt="meter"
